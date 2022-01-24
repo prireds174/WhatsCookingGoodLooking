62c906c7b43812529a553c0f427c59152bd448a8
maccam91 <michael.cortezjr@gmail.com>
2022-01-24 10:26:38 -0600

    added controllers/models/views/ and basic express setup

---
+++ server.js
@@ -0,0 +1,41 @@
+// ============================================
+//                   DEPENDENCIES
+// ============================================
+const express = require('express')
+const app = express()
+const PORT = 8000
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const URI = "mongodb://127.0.0.1:27017/easypeasy"
+mongoose.connect(URI, {}, ()=> console.log("mongoose connected!" +URI))
+
+
+// ============================================
+//                   CONFIGURATION
+// ============================================
+app.set('view engine', 'ejs')
+app.use('/views', express.static('views'))
+
+
+
+
+// ============================================
+//                   MIDDLEWARE
+// ============================================
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+// ============================================
+//                   7 "RESTful" routes 
+// ============================================
+
+
+
+// ***************Home*Route*********************
+
+
+
+
+
+// ***************Start*Server*******************
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
